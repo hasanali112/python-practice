005cbc8cee2e68ac0d3528ddc4ce49ef6e21e629
hasanali112 <sohagali.ru.ac@gmail.com>
2025-06-21 22:01:49 +0600

    finsh 5 video

--- Data type/memory/memory.py
+++ Data type/memory/memory.py
@@ -1,13 +1,25 @@
-a= 10
-b=12
+# a= 10
+# b=12
 
-print(id(a))
+# print(id(a))
 
-print(id(b))
+# print(id(b))
 
-c= "Rahim"
-print(c)
-c= list(c)
-c[0] = 'F'
-c = "".join(c) # for loop er moto kaj kore
-print(c)
+# c= "Rahim"
+# print(c)
+# c= list(c)
+# c[0] = 'F'
+# c = "".join(c) # for loop er moto kaj kore
+# print(c)
+
+
+# list 
+
+a="madam"
+print(a== a[::-1])
+
+
+lst = [1,2,3,4,5]
+
+dic = {item: item**2 for item in lst}
+print(dic)
---
+++ Video-01/01_basices/hello_chai.py
@@ -0,0 +1,14 @@
+print("hello chai")
+
+
+def chai(n):
+    print(n)
+
+chai(4)
+
+ami_chai = "masala chai"
+chai_2 = "black chai"
+chai_3 = "green chai"
+
+
+### finish hitesh chai 5 video
